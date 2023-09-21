c6d9192b37cbca64111e488e89bb130a35bde55e
Autumn-leave <meenachivalliammai@gmail.com>
2023-09-22 04:45:40 +0530

    dashboard front & back

--- Front/blog_app/src/Router/Routers.js
+++ Front/blog_app/src/Router/Routers.js
@@ -18,15 +18,10 @@ const Routers = () => {
         <Routes>
             <Route path='/Sign_in' element={<Sign_in />} />
             <Route path='/Sign_up' element={<Sign_up />} />
-<<<<<<< HEAD
             <Route path='/' element={<Dashboard_home_page />} />
             <Route path='/Dashboard_new_page' element={<Dashboard_new_page />} />
             <Route path='/Dashboard_edit_page' element={<Dashboard_edit_page />} />
             
-=======
-            <Route path='/Dummy' element={<Dummy />} />
-            <Route path='/' element={<Getdummy />} />
->>>>>>> cf06fb1851b0f92b13b2f1dba025eb34947753b9
         </Routes>
         </BrowserRouter>
     );
